7a894d303da791b61571883f55c2cf08b381522f
zinetin <zinetin2050@proton.me>
2026-07-04 15:49:29 +0100

    fixes

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, hyprland, ... }:
+  outputs = { self, nixpkgs, ... }:
   let
     forAllSystems = nixpkgs.lib.genAttrs [
       "x86_64-linux"
